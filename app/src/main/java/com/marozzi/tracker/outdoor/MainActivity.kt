c3c6560394089c363a50160cf6d7c7c032e1cae9
Andrea <andmaroz89@gmail.com>
2019-09-20 08:55:02 +0200

    create project

---
+++ app/src/main/java/com/marozzi/tracker/outdoor/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marozzi.tracker.outdoor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
